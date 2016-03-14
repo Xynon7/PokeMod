98cc17512610100e74690881d4ec2333da497cca
Unowndeveloper <unowndeveloper@gmail.com>
2016-03-14 13:44:14 +1100

    Added files via upload

---
+++ Sounds/NPCKilled/id30.cs
@@ -0,0 +1,18 @@
+using Microsoft.Xna.Framework.Audio;
+using Terraria;
+using Terraria.ModLoader;
+
+namespace PokeModRed.Sounds.NPCKilled
+{
+	public class id30 : ModSound
+	{
+		public override void PlaySound(ref SoundEffectInstance soundInstance, float volume, float pan, SoundType type)
+		{
+			soundInstance = sound.CreateInstance();
+			soundInstance.Volume = volume;
+			soundInstance.Pan = pan;
+			soundInstance.Pitch = 0.8f;
+			Main.PlaySoundInstance(soundInstance);
+		}
+	}
+}
